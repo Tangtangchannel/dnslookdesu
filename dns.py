392e7d2346cc8eb3b83f7befb766d1cad17c38ae
tangtang <gallagherabigayle584@gmail.com>
2023-08-09 20:48:32 +0800

    new push

--- dns.py
+++ dns.py
@@ -1,87 +1,89 @@
+import argparse
 import requests
 
-# 设置 Cloudflare 的 DoH URL
-doh_url = "https://cloudflare-dns.com/dns-query"
+def main():
+    parser = argparse.ArgumentParser(description="使用 Cloudflare 的 DoH 服务查询 DNS 记录")
+    parser.add_argument("domain", type=str, help="要查询的域名")
+    parser.add_argument("-t", "--type", type=str, help="DNS 记录类型 (A, NS, CNAME, MX, TXT, AAAA, SRV, CAA, HTTPS, SVCB, SPF)")
+    parser.add_argument("-d", "--dns", type=str, help="要查询的 DNS 服务器（可选）")
+    args = parser.parse_args()
 
-print("输入请求域名：")
-satou_host = input()
+    doh_url = "https://cloudflare-dns.com/dns-query"
 
-# 定义不同记录类型的映射关系
-record_type_map = {
-    "A": 1,
-    "NS": 2,
-    "CNAME": 5,
-    "MX": 15,
-    "TXT": 16,
-    "AAAA": 28,
-    "SRV": 33,
-    "CAA": 257,
-    "HTTPS": 65,
-    "SVCB": 64,
-    "SPF": 99,
-}
+    if args.dns:
+        doh_url = f"https://{args.dns}/dns-query"
 
-# 获取用户输入的记录类型
-print("输入记录类型（A、NS、CNAME等）：")
-record_type_input = input()
-record_type = record_type_input.upper()
+    domain = args.domain
+    record_type_input = args.type.upper() if args.type else None
 
-# 检查记录类型是否有效
-if record_type not in record_type_map:
-    print("无效的记录类型")
-else:
-    # 构建 DoH 查询参数
-    params = {
-        "name": satou_host,
-        "type": record_type_map[record_type],
+    record_type_map = {
+        "A": 1,
+        "NS": 2,
+        "CNAME": 5,
+        "MX": 15,
+        "TXT": 16,
+        "AAAA": 28,
+        "SRV": 33,
+        "CAA": 257,
+        "HTTPS": 65,
+        "SVCB": 64,
+        "SPF": 99,
     }
 
-    # 设置请求头
-    headers = {
-        "Accept": "application/dns-json",
-    }
-
-    # 发起 GET 请求并获取响应
-    response = requests.get(doh_url, params=params, headers=headers)
-
-    # 解析 JSON 响应
-    if response.status_code == 200:
-        data = response.json()
-        answers = data.get("Answer", [])
-
-        for answer in answers:
-            print("Type:", answer["type"])
-            print("Name:", answer["name"])
-            print("TTL:", answer["TTL"])
-
-            # 根据记录类型处理数据
-            if record_type == "A" or record_type == "AAAA":
-                print("Data:", answer.get("data", ""))
-            elif record_type == "NS":
-                print("Data: NS record:", answer.get("data", ""))
-            elif record_type == "CNAME":
-                print("Data: CNAME record:", answer.get("data", ""))
-            elif record_type == "MX":
-                print("Data: MX record:", answer.get("data", ""))
-            elif record_type == "TXT":
-                print("Data: TXT record:", answer.get("data", ""))
-            elif record_type == "SRV":
-                srv_data = answer.get("data", "")
-                srv_parts = srv_data.split()
-
-                if len(srv_parts) >= 4:
-                    priority = srv_parts[0]
-                    weight = srv_parts[1]
-                    port = srv_parts[2]
-                    target = srv_parts[3]
-
-                    print("Data: SRV record")
-                    print("Priority:", priority)
-                    print("Weight:", weight)
-                    print("Port:", port)
-                    print("Target:", target)
-                else:
-                    print("Invalid SRV data format")
-
+    if record_type_input and record_type_input not in record_type_map:
+        print("无效的记录类型")
     else:
-        print("Error:", response.status_code)
+        record_type = record_type_input or "A"
+        params = {
+            "name": domain,
+            "type": record_type_map[record_type],
+        }
+
+        headers = {
+            "Accept": "application/dns-json",
+        }
+
+        response = requests.get(doh_url, params=params, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            answers = data.get("Answer", [])
+
+            for answer in answers:
+                print("类型:", answer["type"])
+                print("名称:", answer["name"])
+                print("TTL:", answer["TTL"])
+
+                if record_type == "A" or record_type == "AAAA":
+                    print("数据:", answer.get("data", ""))
+                elif record_type == "NS":
+                    print("数据: NS 记录:", answer.get("data", ""))
+                elif record_type == "CNAME":
+                    print("数据: CNAME 记录:", answer.get("data", ""))
+                elif record_type == "MX":
+                    print("数据: MX 记录:", answer.get("data", ""))
+                elif record_type == "TXT":
+                    print("数据: TXT 记录:", answer.get("data", ""))
+                elif record_type == "SRV":
+                    srv_data = answer.get("data", "")
+                    srv_parts = srv_data.split()
+
+                    if len(srv_parts) >= 4:
+                        priority = srv_parts[0]
+                        weight = srv_parts[1]
+                        port = srv_parts[2]
+                        target = srv_parts[3]
+
+                        print("数据: SRV 记录")
+                        print("优先级:", priority)
+                        print("权重:", weight)
+                        print("端口:", port)
+                        print("目标:", target)
+                    else:
+                        print("无效的 SRV 数据格式")
+
+        else:
+            print("错误:", response.status_code)
+
+if __name__ == "__main__":
+    main()
